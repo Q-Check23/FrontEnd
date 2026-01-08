4580ebce3adea6da684f8def3f701f14da6f74a7
yaho <565kseojin23@gmail.com>
2026-01-08 14:31:40 +0900

    Feat: React Calendar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import Calender from "react-calendar";
 
 export default function Home() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>홈페이지</h1>
+    <div>
+    <Calender/>
      
     </div>
   );
