8b03e104cae5767537192845225a53606a47378f
jyun27 <kimjyun27@gmail.com>
2026-05-07 17:16:22 +0900

    feat: 유저 스토어에 accessToken 메모리 저장소 추가

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -3,12 +3,16 @@ import type { MyProfile } from "../api/users";
 
 interface UserState {
   profile: MyProfile | null;
+  accessToken: string | null;
   setProfile: (profile: MyProfile) => void;
+  setAccessToken: (token: string | null) => void;
   clear: () => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   profile: null,
+  accessToken: null,
   setProfile: (profile) => set({ profile }),
-  clear: () => set({ profile: null }),
+  setAccessToken: (accessToken) => set({ accessToken }),
+  clear: () => set({ profile: null, accessToken: null }),
 }));
